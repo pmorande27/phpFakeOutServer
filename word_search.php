d8fe8089c8dca8fb037f4c8c0c06dda770912cd2
pmorande27 <pmorande27@gmail.com>
2020-04-25 16:22:54 +0200

    syntax

--- word_search.php
+++ word_search.php
@@ -9,7 +9,7 @@ $result = $connexion -> query($query);
 while($row = mysqli_fetch_assoc($result)){
 	$product[] = array_map('utf8_encode',$row);
 }
-if (!mysqli_fetch_assoc($result))){
+if (!mysqli_fetch_assoc($result)){
 	echo json_encode (json_decode ("{}")
 }
 else{
