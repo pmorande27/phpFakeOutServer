49bb3f2709d28389706b27d1017c09b03bff26f4
pmorande27 <pmorande27@gmail.com>
2020-04-25 16:30:18 +0200

    changed contains to like

--- word_search.php
+++ word_search.php
@@ -3,7 +3,7 @@ include 'connexion.php';
 //$word = $_POST['word'];
 $word = "Fact";
 //%word = &_GET['word'];
-$query = "SELECT SOURCE from WhiteList where INFORMATION LIKE '%Fact%' ";
+$query = "SELECT SOURCE from WhiteList where INFORMATION LIKE '%Fact:%' ";
 $result = $connexion -> query($query);
 while($row = mysqli_fetch_assoc($result)){
 	$product[] = array_map('utf8_encode',$row);
