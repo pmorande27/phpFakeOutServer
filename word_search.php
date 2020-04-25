78fe62232529abd27e793a6707a2759c6fdbe519
pmorande27 <pmorande27@gmail.com>
2020-04-25 16:21:54 +0200

    testing word_search.php

--- word_search.php
+++ word_search.php
@@ -1,18 +1,19 @@
 <?php 
 include 'connexion.php';
-$word = $_POST['word'];
+//$word = $_POST['word'];
+$word = "Fact";
 //%word = &_GET['word'];
-$query = "SELECT INFORMATION from WhiteList where SOURCE LIKE '%$word%' ";
+$query = "SELECT SOURCE from WhiteList where INFORMATION LIKE '%$word%' ";
 //$query = "SELECT text from TEST where SOURCE CONTAINS '$word' ";
 $result = $connexion -> query($query);
 while($row = mysqli_fetch_assoc($result)){
 	$product[] = array_map('utf8_encode',$row);
 }
-if (!empty($product)){
-	echo json_encode($product);
+if (!mysqli_fetch_assoc($result))){
+	echo json_encode (json_decode ("{}")
 }
 else{
-	echo json_encode (json_decode ("{}"));
+	echo json_encode ($product);
 }
 $connexion->close();
  ?>
