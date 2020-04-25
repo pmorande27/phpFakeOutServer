f310098881c22114b627da517fa726f86c589931
pmorande27 <pmorande27@gmail.com>
2020-04-25 16:32:02 +0200

    syntax

--- word_search.php
+++ word_search.php
@@ -5,15 +5,18 @@ $word = "Fact";
 //%word = &_GET['word'];
 $query = "SELECT SOURCE from WhiteList where INFORMATION LIKE '%Fact:%' ";
 $result = $connexion -> query($query);
-while($row = mysqli_fetch_assoc($result)){
-	$product[] = array_map('utf8_encode',$row);
-}
 if (!mysqli_fetch_assoc($result)){
 	echo json_encode (json_decode ("{}"));
 }
 else{
-	echo json_encode ($product);
+while($row = mysqli_fetch_assoc($result)){
+	$product[] = array_map('utf8_encode',$row);
 }
+
+	echo json_encode ($product);
+
+}
+
 $connexion->close();
  ?>
 
