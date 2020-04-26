53655d81c24abd52a236c8dfb1b003356c3f38c9
pmorande27 <pmorande27@gmail.com>
2020-04-26 15:30:22 +0200

    finished testing of word_search

--- word_search.php
+++ word_search.php
@@ -1,17 +1,15 @@
 <?php 
 include 'connexion.php';
-//$word1 =  $GET['word1'];
-
-//$word2 =  $GET['word2'];
-//$word3 =  $GET['word3'];
-//$word4 =  $GET['word4'];
-//$word5 =  $GET['word5'];
-$word1 =  "University";
-$word2 =  "out";
-$word3 =  "Johns";
-$word4 = "has";
-$word5 =  "public";
-//$word = "edjtgwogrjeoijhoerhgwoighegoirhOGWHRVOLDJIGOIEWJHGOWEHRJOGisdjofgiehwoigfhweoghjoivji";
+$word1 =  $GET['word1'];
+$word2 =  $GET['word2'];
+$word3 =  $GET['word3'];
+$word4 =  $GET['word4'];
+$word5 =  $GET['word5'];
+//$word1 =  "University";
+//$word2 =  "out";
+//$word3 =  "Johns";
+//$word4 = "has";
+//$word5 =  "public";
 if (isset($word1) && isset($word2) && isset($word3) && isset($word4) && isset($word5) ){
 //$word1 = $wor1 . " ";
 //$word2 = $wor2 . " ";
