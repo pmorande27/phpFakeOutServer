d79236c854ec3dc34f0b9e9850e767900a85b717
pmorande27 <pmorande27@gmail.com>
2020-04-27 04:04:53 +0200

    add_whiteList

---
+++ add_whitelist.php
@@ -0,0 +1,27 @@
+<?php
+include 'connexion.php';
+$url = $_GET['url'];
+$text = $_GET['text'];
+$title = $_GET['title'];
+$query = "INSERT INTO WhiteList (SOURCE,TITLE,INFORMATION) VALUES ('$url','$title',$text')";
+if (isset($url)&&isset($text)&&isset($title)){
+	$result = $connexion->query($query);
+if($result){
+	$return = Array();
+    $return["success"] = true;
+    echo json_encode($return);
+
+}
+else{
+	$return = Array();
+    $return["success"] = true;
+    echo json_encode($return);
+
+}
+}
+else{
+	echo json_encode(json_decode('{}'));
+}
+$connexion->close();
+
+?>
