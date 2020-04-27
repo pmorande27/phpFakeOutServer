c9e7564b25585bc9545deeb978eacfd5925a1df7
pmorande27 <pmorande27@gmail.com>
2020-04-27 04:28:27 +0200

    a

--- add_whitelist.php
+++ add_whitelist.php
@@ -2,12 +2,10 @@
 header('Access-Control-Allow-Origin: *');
 
 include 'connexion.php';
-//$url = $_GET['url'];
-//$text = $_GET['text'];
-//$title = $_GET['title'];
-$url = "aaa";
-$text = "aaa";
-$title = "dgg";
+$url = $_GET['url'];
+$text = $_GET['text'];
+$title = $_GET['title'];
+
 $query = "INSERT INTO WhiteList (SOURCE,TITLE,INFORMATION) VALUES ('$url','$title','$text')";
 if (isset($url)&&isset($text)&&isset($title)){
 	$result = $connexion->query($query);
